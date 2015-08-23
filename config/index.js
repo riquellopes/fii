db547c7a00e3f54f97936f1e4838676c5196bee3
Henrique Lopes <riquellopes@gmail.com>
2015-08-23 13:52:41 -0300

    Adicionando tratamento para env

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
-try{
+if( process.env.URI == undefined ){
     require('dotenv').config({silent: true, path: process.env.NODE_ENV || ".env" });
     require('dotenv').load();
-}catch(e){/***/}
+}
 
 module.exports = {
     "uri": process.env.URI,
